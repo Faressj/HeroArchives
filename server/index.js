79279c1d930a14d39c947d2c8cb11c937c897ed5
Fares <faresk1407@gmail.com>
2023-11-17 04:54:47 +0100

    try new indexjs

--- server/index.js
+++ server/index.js
@@ -9,33 +9,12 @@ require('dotenv').config({ path: '../.env' });
 const publicKey = process.env.PUBLIC_KEY;
 const privateKey = process.env.PRIVATE_KEY;
 
-// app.use(cors({
-//     origin: 'https://heroarchives.com'
-// }));
-// const allowedOrigins = ['https://heroarchives.com', 'https://www.heroarchives.com'];
-// app.use(cors({
-//     origin: function(origin, callback){
-//         if(!origin || allowedOrigins.indexOf(origin) !== -1){
-//             callback(null, true);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     }
-// }));
+app.use(cors({
+    origin: 'https://heroarchives.com'
+}));
 
-if (process.env.NODE_ENV === "production") {
-    // Modifiez ce chemin selon l'emplacement de votre frontend buildé
-    app.use(express.static('/home/wami5019/HeroArchives/dist'));
-}
-
-console.log(__dirname);
 app.get('/', (req, res) => {
-    if (process.env.NODE_ENV === "production") {
-        res.sendFile(path.join('/home/wami5019/HeroArchives/dist/index.html'));
-    } else {
-        res.send('Server is running');
-    }
-
+    res.send('Server is running');
 });
 
 app.get('/search-hero', async (req, res) => {
@@ -43,7 +22,7 @@ app.get('/search-hero', async (req, res) => {
     const hash = crypto.createHash('md5').update(ts + privateKey + publicKey).digest('hex');
     const query = req.query.name;
     try {
-        const response = await axios.get(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${query}&ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=10`); // Ajout d'une limite
+        const response = await axios.get(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${query}&ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=10`); // Ajout d'une limite
         res.json(response.data);
     } catch (error) {
         res.status(500).send('Error fetching data from Marvel API');
@@ -56,7 +35,7 @@ app.get('/hero/:id', async (req, res) => {
     const hash = crypto.createHash('md5').update(ts + privateKey + publicKey).digest('hex');
 
     try {
-        const response = await axios.get(`http://gateway.marvel.com/v1/public/characters/${heroId}?ts=${ts}&apikey=${publicKey}&hash=${hash}`);
+        const response = await axios.get(`https://gateway.marvel.com/v1/public/characters/${heroId}?ts=${ts}&apikey=${publicKey}&hash=${hash}`);
         res.json(response.data);
     } catch (error) {
         res.status(500).send('Error fetching hero data from Marvel API');
@@ -68,7 +47,7 @@ app.get('/comic/:comicId', async (req, res) => {
     const hash = crypto.createHash('md5').update(ts + privateKey + publicKey).digest('hex');
 
     try {
-        const response = await axios.get(`http://gateway.marvel.com/v1/public/comics/${comicId}?ts=${ts}&apikey=${publicKey}&hash=${hash}`);
+        const response = await axios.get(`https://gateway.marvel.com/v1/public/comics/${comicId}?ts=${ts}&apikey=${publicKey}&hash=${hash}`);
         res.json(response.data);
     } catch (error) {
         res.status(500).send('Error fetching data from Marvel API');
@@ -80,7 +59,7 @@ app.get('/serie/:serieId', async (req, res) => {
     const hash = crypto.createHash('md5').update(ts + privateKey + publicKey).digest('hex');
 
     try {
-        const response = await axios.get(`http://gateway.marvel.com/v1/public/series/${serieId}?ts=${ts}&apikey=${publicKey}&hash=${hash}`);
+        const response = await axios.get(`https://gateway.marvel.com/v1/public/series/${serieId}?ts=${ts}&apikey=${publicKey}&hash=${hash}`);
         res.json(response.data);
     } catch (error) {
         res.status(500).send('Error fetching data from Marvel API');
@@ -94,7 +73,7 @@ app.get('/story/:storyId', async (req, res) => {
     const hash = crypto.createHash('md5').update(ts + privateKey + publicKey).digest('hex');
 
     try {
-        const response = await axios.get(`http://gateway.marvel.com/v1/public/stories/${storyId}/series?ts=${ts}&apikey=${publicKey}&hash=${hash}`);
+        const response = await axios.get(`https://gateway.marvel.com/v1/public/stories/${storyId}/series?ts=${ts}&apikey=${publicKey}&hash=${hash}`);
         res.json(response.data);
     } catch (error) {
         res.status(500).send('Error fetching data from Marvel API');
@@ -107,17 +86,13 @@ app.get('/event/:eventId', async (req, res) => {
     const hash = crypto.createHash('md5').update(ts + privateKey + publicKey).digest('hex');
 
     try {
-        const response = await axios.get(`http://gateway.marvel.com/v1/public/events/${eventId}?ts=${ts}&apikey=${publicKey}&hash=${hash}`);
+        const response = await axios.get(`https://gateway.marvel.com/v1/public/events/${eventId}?ts=${ts}&apikey=${publicKey}&hash=${hash}`);
         res.json(response.data);
     } catch (error) {
         res.status(500).send('Error fetching data from Marvel API');
     }
 });
-if (process.env.NODE_ENV === "production") {
-    app.get('*', (req, res) => {
-        res.sendFile('/home/wami5019/HeroArchives/dist/index.html');
-    });
-}
+
 app.listen(port, () => {
-    console.log(`Server is listening on http://localhost:${port}`);
+    console.log(`Server listening at https://heroarchives.com:${port}`);
 });
